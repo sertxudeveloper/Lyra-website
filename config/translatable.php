1bf5a3cf1a2b5f408080522e5938347d2d2e6823
Sertxu Developer <dev.sertxu@gmail.com>
2020-05-23 12:37:28 +0200

    Configured laravel-translatable

---
+++ config/translatable.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+
+    /**
+     * The locales available in your project.
+     */
+    "locales" => ["en", "es"],
+
+    /**
+     * The default locale in your project, it will be used if the requested locale is not available.
+     */
+    "fallback_locale" => "en",
+
+    /**
+     * Hide the default locale from the URL.
+     */
+    "hide_fallback_locale" => false,
+
+    /**
+     * The translations table suffix
+     */
+    "table_suffix" => "_translations"
+];
