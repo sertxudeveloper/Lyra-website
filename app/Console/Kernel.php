de12d41ea8f88239504371857117640dcbee9fee
Sergio Peris <dev.sertxu@gmail.com>
2021-12-25 21:17:12 +0100

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('demo:reset --force')->daily();
+        $schedule->command('demo:reset')->daily();
     }
 
     /**
